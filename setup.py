f3c0c651b30e30043f4fdeeae2c6ab054839306d
Cem Gokmen <cem@cemgokmen.com>
2018-09-24 15:33:55 -0400

    Bump version: 0.3.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/zucchini/pyLC3',
-    version='0.3.0',
+    version='version='0.3.0'',
     zip_safe=False,
 )
